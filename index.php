6ab66a8725f68417c921052344bfce14b09d9c50
akusha <akusha260@gmail.com>
2017-02-12 03:52:08 +0300

    вход
    новая страница для входа.

--- index.php
+++ index.php
@@ -2,72 +2,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Рабочий стол</title>
+        <title>учет</title>
         <link rel="shortcut icon" href="image/logo_100_100.png" type="image/x-icon">
         <meta charset="utf-8">
-        <meta name="description" content="Рабочий стол, Стартовая страница, Домашняя страница">
-        <meta name="keywords" content="Рабочий стол, стартовая страница, доманшяя страница, пусани">
+        <meta name="description" content="Учет рабочего времени, рабочий процесс">
+        <meta name="keywords" content="работы, табель, часы, рабочий день, зарплата">
         <link rel="stylesheet" href="index.css">
-        <script src="index.js"></script>
-        <script src="jquery-3.1.1.js"></script>
+        <script src="index.js"></script>        
     </head>
     <body> 
-        <header class="header">
-            <a href="http://localhost"><img src="image/logo_100_100.png" class="logo"/></a>            
-		<nav>
-            <ul class = "topmenu">
-                <li>Рассказы
-                    <ul class = "submenu">
-                        <li>роман</li>
-                        <li>рассказ</li>
-                        <li>Стихи</li>
-                    </ul>
-                </li>
-                <li><a href="https://ok.ru">odnok</a></li>
-                <li><a href="https://www.youtube.com/">youtube</a></li>
-                <li><a href="https://www.facebook.com/">facebook</a></li>
-                <li><a href="https://vk.com">vk</a></li>
-                <li><a href="https://www.hedbed.ru/check_in.html">skype</a></li>   
-            </ul>
-        </nav>
+        <header>
+            <div class="head"><span>Добро пожаловать на сайт учета работников.</span></div>   
+            <?php
+                if(isset($_COOKIE['log'])) echo "<div class='login'><a href='cabinet.php'>".$_COOKIE['log']."</a> </div>"; 
+                else echo "<div class='login'><a href='login.php'> вход </a> </div>";
+            ?>
         </header>
-        <?php
-            if(isset($_COOKIE['log'])) echo "<div class='login'><a href='cabinet.php'>".$_COOKIE['log']."</a> </div>"; 
-            else echo "<div class='login'><a href='login.php'> вход </a> </div>";
-        ?>
-        
-        <!-- <script src="index.js" defer></script> -->
-         <div class="container">
-            <div class="leftcol">
-                <li><span>..........</span></li>                        
-                <li><a href="#g1">Глава1 <b>Встреча.</b></a></li>
-                <li><a href="#g2">Глава2 <b>Знакомство.</b></a></li>
-                <li><a href="#g3">Глава3 <b>Привязывание.</b></a></li>
-                <li><a href="#g4">Глава4 <b>Горячка.</b></a></li>
-                <li><a href="#g5">Глава5 <b>Любовь.</b></a></li>                     
-                               
-            </div>
-            <div class="rightcol">                
-                <h1>...........</h1>                
-                <h2 class="rt" id = "g1">Встреча, (или самое начало)</h2>
-                <p>Ничто ничего не предвещало, по крайней мере я так думаю...</p>
-                <p>Так себе, еще один день... Замечательна она тем что я в тот день я повстречал мою любовь.</p>
-                <p>Возвращаясь мысленно в то время, я не могу воскресить те чувства, что я тогда испытывал, не знаю почему... Если только 
-                   восстановить картину по памяти, хотя и от него не много осталось...</p>
-                <p id="text">Постараюсь быть предельно честым.. т.е. постараюсь на сколько это возможно не преукращивать внутренние картины.</p>
-                <p>Первый раз когда я ее увидел, она мне напомнила кое кого, не знаю почему. Хотя я увидел ее из далека, и не мог рассмотреть.
-                   Когда же нас познакомили, и я увидел ее вблизи, вот что я испытал... </p>
-                <p>-Да все.... я сам не знаю что.... подумать надо, что б понять.. что б уяснить....</p>
-                <p>Пожалуй она казалась необычной.. </p><p></p><p></p><p></p>
-                <button type="button" onclick="fTest()">Изменить</button>
-            </div>
-        </div>
-        <footer>
-        <div>
-            <a href="mailto:akusha260@gmail.com?subject=вопрос к администратору">&#9993 вопрос к админ.</a>
-            Магомедов Магомед Кадиевич.
-        </div>
-        </footer>
-
     </body>
 </html>
--- login.php
+++ login.php
@@ -9,14 +9,8 @@ if (isset($_COOKIE['log'])){
 
     if (isset($_POST['log'])){
             $log = $_POST["log"];
-
-
             $pass = $_POST["pass"];
-        
             include "conectSQL.php";
-        
-      
-
             $stmt = $mysqli->stmt_init();
             if(($stmt->prepare("SELECT * FROM users WHERE log=? AND pass=?") === FALSE)
                 or ($stmt->bind_param('ss', $log, $pass) === FALSE)
@@ -35,74 +29,25 @@ if (isset($_COOKIE['log'])){
             $result->close();
             $mysqli->close();
     }
-        
-           
         ?>    
 <!DOCTYPE html>
 <html>
     <head>
         <title>вход</title>
         <link rel="shortcut icon" href="image/logo_100_100.png" type="image/x-icon">
+        <link rel="stylesheet" href="login.css">
         <meta charset="utf-8">    
-        <script src="jquery-3.1.1.js"></script>
-        <style> 
-            body{
-                background: #b7b2b2;
-                
-            }
-            .view {
-                    padding: 5px;
-                    width: 250px;                    
-                    position: absolute;
-                    top: 50%;
-                    left: 50%;
-                    margin: -50px 0 0 -125px;                    
-                    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.25), 7px 7px 5px rgba(0, 0, 0, 0.22);
-                    background: #453e3e;
-                text-align: center;
-            }
-            fieldset{
-                align-content: space-around;
-            }
-            legend {
-                text-align: left;
-            }
-            input[type="text"] {
-                
-            }
-            input {
-                background: #a1c7a1;
-                border: 1px solid #616184;
-            }
-            label {
-                width: 110px;
-                display: inline-block;
-                vertical-align: top;
-                margin: 6px;
-            }
-            input:focus {
-                background: #98ed8d;
-                border: 1px solid #18671b;
-            }
-            input:required:invalid {
-	           border:1px solid red;
-            }
-            ::-webkit-input-placeholder {color:#ac6c65;}
-            ::-moz-placeholder          {color:#ac6c65;}/* Firefox 19+ */
-            :-moz-placeholder           {color:#ac6c65;}/* Firefox 18- */
-            :-ms-input-placeholder      {color:#ac6c65;}
-        </style>
     </head>
     <body>
-        <div class="view">
-            <form method="post">
-                <fieldset>
-                    <legend>вход</legend>
-                    <label for="log">логин:</label><input type="text" name="log" placeholder="akusha@mail.ru" required autofocus>
-                    <label for="pass">пароль:</label><input type="password" name="pass" required>
-                </fieldset>    
-                    <input type="submit" value="войти">
-            </form>
-        </div>
-    </body>
+    <div id="login">
+        <form method="POST">
+            <fieldset class="clearfix">
+                <p><span class="fontawesome-user"></span><input type="text" name="log" value="Логин" onBlur="if(this.value == '') this.value = 'Логин'" onFocus="if(this.value == 'Логин') this.value = ''" required></p> 
+                <p><span class="fontawesome-lock"></span><input type="password" name="pass"  value="Пароль" onBlur="if(this.value == '') this.value = 'Пароль'" onFocus="if(this.value == 'Пароль') this.value = ''" required></p> 
+                <p><input type="submit" value="ВОЙТИ"></p>
+            </fieldset>
+        </form>
+        <p>Нет аккаунта? &nbsp;&nbsp;<a href="check_in.php">Регистрация</a><span class="fontawesome-arrow-right"></span></p>
+    </div>
+</body>
 </html>
