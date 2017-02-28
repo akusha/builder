6b50f062708850e22d2d73ff9858aa74b5e99a94
akusha <akusha260@gmail.com>
2017-03-01 00:08:23 +0300

    новый

---
+++ site.js
@@ -0,0 +1,52 @@
+var menu = document.getElementsByClassName('menu')[0],
+left_menu = document.getElementsByClassName('left_menu')[0];
+
+
+window.onscroll = function() {
+	var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+	menu.classList.toggle('fixed',scrolled>=100);
+	// left_menu.classList.toggle('fixedleft',scrolled>=100);
+
+	if (scrolled<=100){
+		left_menu.style.top= 150-scrolled+'px';
+	}else 
+		left_menu.style.top = 50+'px';	
+}
+
+
+
+
+var bool = false;
+
+
+
+document.getElementById('new_item').onclick = function (){
+
+	if (bool = !bool)
+		document.getElementById("visib").classList.remove('visib');
+	else
+		document.getElementById("visib").classList.add('visib');
+}
+
+//window.onresize = function() {
+//	document.getElementById("widhei").innerHTML = document.documentElement.clientWidth;
+//}
+
+
+
+window.onload = function(){
+	
+	var li = document.getElementsByClassName('item');
+	
+	for (var i=0;i<li.length;i++){
+		
+		li[i].onclick = function (){
+			
+			document.getElementsByClassName('conteiner')[0].innerHTML = this.getAttribute('value2');
+			
+		}
+		
+	}	
+	
+}
---
+++ site.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Делаем сайты</title>
+        <link rel="shortcut icon" href="image/logo_100_100.png" type="image/x-icon">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="site.css">
+        <meta charset="utf-8">    
+    </head>
+    <body>
+    	<div class="logo">
+<!--			<img src="image/logo.jpg">-->
+			<div class ="log1">
+				<h1>Сайты на любой вкус.</h1><br>
+			</div>
+			
+			<div class ="log2">
+				<span class="span">hedbed.ru</span>
+			</div>
+					
+			<div class="log3">
+				<p>тел. 8(965)714-44-55</p>
+			</div>
+			<div class="menu">
+				<nav>
+					<li id="new_item" class="fontawesome-reorder"></li>
+					<li><a href="#img1">разработка сайта</a></li>
+					<li><a href="#img2">раскрутка инстаграмм</a></li>
+					<li><span>реклама в интеренете</span></li>
+					<li><span>контакты</span></li>
+				</nav>
+			</div>
+    	</div>
+    	
+    	
+    	<div class="conteiner">
+			<div>
+			<img src="image/mockup_logo.jpg" id="img1">
+			<div class="head_h2">
+				<h2>
+					Разработать сайт - это то что будем делать мы, вы занимайтесь своим делом.)))
+				</h2>
+			</div>
+			</div>
+			<div>
+			<img src="image/hedbed.jpg" id="img2">
+			<div class="head_h2">
+				<h2>
+					Раскрутить инстаграмм, да в два счета.))) Только на деньги не скупитесь.))
+				</h2>
+			</div>
+			</div>
+			
+    	</div>
+
+
+
+    	
+       <script src="site.js"/></script>
+    </body>
+</html>
