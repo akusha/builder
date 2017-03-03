b97f9aeaa4ba2670c04da54fbeca66252c76cad4
akusha <akusha260@gmail.com>
2017-03-03 19:50:47 +0300

    кук

--- site.php
+++ site.php
@@ -2,14 +2,16 @@
 <html>
     <head>
         <title>Делаем сайты</title>
-        <link rel="shortcut icon" href="image/logo_100_100.png" type="image/x-icon">
+        <link rel="shortcut icon" href="image/logo_hb.jpg" type="image/x-icon">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="site.css">
         <meta charset="utf-8">    
     </head>
     <body>
     	<div class="logo">
-<!--			<img src="image/logo.jpg">-->
+			<div class ="logos">
+				<img src="image/logo_400_300.png">
+			</div>
 			<div class ="log1">
 				<h1>Сайты на любой вкус.</h1><br>
 			</div>
@@ -35,20 +37,21 @@
     	
     	<div class="conteiner">
 			<div>
-			<img src="image/mockup_logo.jpg" id="img1">
-			<div class="head_h2">
-				<h2>
-					Разработать сайт - это то что будем делать мы, вы занимайтесь своим делом.)))
-				</h2>
+				<img src="image/mockup_logo.jpg" id="img1">
+				<div class="head_h2">
+					<h2>
+						Разработать сайт - это то что будем делать мы, вы занимайтесь своим делом.)))
+					</h2>
+				</div>
 			</div>
-			</div>
+			
 			<div>
-			<img src="image/hedbed.jpg" id="img2">
-			<div class="head_h2">
-				<h2>
-					Раскрутить инстаграмм, да в два счета.))) Только на деньги не скупитесь.))
-				</h2>
-			</div>
+				<img src="image/hedbed.jpg" id="img2">
+				<div class="head_h2">
+					<h2>
+						Раскрутить инстаграмм, да в два счета.))) Только на деньги не скупитесь.))
+					</h2>
+				</div>
 			</div>
 			
     	</div>
